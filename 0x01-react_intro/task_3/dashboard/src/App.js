e576a91659eea1a82f5b1017aed9b02d31f8df51
mbt371 <57286135+mbt371@users.noreply.github.com>
2023-03-19 21:52:21 +0300

    0x01-react_intro

--- 0x01-react_intro/task_3/dashboard/src/App.js
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -11,6 +11,11 @@ function App() {
       </div>
       <div className='App-body'>
 				<p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email :</label>
+        <input name="email" type="email" id="email"></input>
+        <label htmlFor="password">Password :</label>
+        <input name="password" type="password" id="password"></input>
+        <button>OK</button>
 			</div>
 			<div className='App-footer'>
 				<p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
